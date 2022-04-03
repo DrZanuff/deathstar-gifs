494d6009d6bbaa02729be6bb8a8a729a43ff5bf3
Ricardo <ricardo.machado.nwi@gmail.com>
2022-04-03 19:24:59 -0300

    Docker Files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true
+  },
   plugins: [
     react({
       babel: {
@@ -21,27 +24,3 @@ export default defineConfig({
     })
   ]
 })
-
-/*
-  plugins: [
-    // For all styled components:
-    // create classnames from fileName and displayName in development
-    react({
-      babel: {
-        presets: ['@babel/preset-typescript'],
-        plugins: [
-          '@babel/plugin-transform-typescript',
-          [
-            'babel-plugin-styled-components',
-            {
-              ssr: false,
-              pure: true,
-              displayName: true,
-              fileName: false,
-            },
-          ],
-        ],
-      },
-    }),
-  ];
-  */
